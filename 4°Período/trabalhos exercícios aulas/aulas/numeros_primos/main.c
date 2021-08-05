fd02b9fabf486dc225d0f84fc71817633e1f1831
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-08-05 20:33:56 -0300

    commit full course

---
+++ 4°Período/trabalhos exercícios aulas/aulas/numeros_primos/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int diferenca, maior, menor;
+    int n1, n2 =0;
+    int matriz[];
+    printf("informe o n1:");
+    scanf("%d", &n1);
+
+    printf("\ninforme o n2:");
+    scanf("%d", &n2);
+
+    if(n1 > n2){
+        maior = n1;
+        menor = n2;
+    }else {
+        maior = n2;
+        menor = n1;
+    }
+
+    int matriz[diferenca];
+    diferenca = n1 - n2;
+
+    for(i=menor;i< maior; i++){
+        for(j=0;j<i;j++){
+            if((j / i) == 1){
+                k++
+            }
+        }
+        if(k ==2){
+            matriz[i] = i;
+            printf("%d é primo", i );
+        }
+        k =0;
+    }
+
+
+
+
+
+    return 0;
+}
