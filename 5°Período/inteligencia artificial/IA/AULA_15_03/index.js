df9a16539ce69a647eb5f4bb3bb3e74da9318615
Rafael Damasceno <rafaeldamasceno2000@gmail.com>
2022-03-16 08:59:08 -0300

    Create index.js

---
+++ 5°Período/inteligencia artificial/IA/AULA_15_03/index.js
@@ -0,0 +1,115 @@
+function produto(x=[], y=[]){
+    let temp = []
+
+    for(let i=0; i<x.length; i++){
+
+        temp.push( parseFloat( x[i]) * parseFloat(y[i]));
+
+        return temp;
+
+    }
+}
+
+
+function quadrados(x=[]){
+    let temp = []
+
+    for(let i=0; i<x.length; i++){
+
+        temp.push( parseFloat( x[i]) * parseFloat(x[i]));
+
+        return temp;
+
+    }
+
+}
+
+
+function somatorio(x=[]){
+    let temp =0;
+
+    for(let i =0;i<x.length;i++){
+        temp += parseFloat(x[i]);
+    }
+
+    return temp;
+}
+
+
+function media(x=[]){
+
+    return ( somatorio(x) / x.length);
+    
+}
+
+
+
+
+
+
+function resultados(x=[], y=[], p=0){
+
+    const resultado1 =( (somatorio(x) * somatorio(y)) / x.length)
+
+    const resultado2 = ((somatorio(x) * somatorio(x) )/ x.length)
+
+    const resultado3 = somatorio(produto(x,y)) - resultado1;
+
+    const resultado4 = resultado3/(somatorio(quadrados(x)) - resultado2);
+
+    const resultado5 = media(y) - (resultado4 * media(x))
+
+
+
+    return ((resultado4 * p) + resultado5).toFixed(0)
+
+}
+
+
+
+function linearRegression(eixoX = [], eixoY = []){
+    const tamX = eixoX.length;
+    const tamY = eixoY.length;
+
+
+    // cria um vetor temporario que vai da posicai 0 de x ate o tamanho de y
+
+    const tempX = eixoX.slice(0, tamY);
+
+    const tempY =  eixoY;
+
+    const dif = tamX - tamY;
+
+
+    if(dif > 0){
+
+        let regressoes = [];
+
+
+        for(let i =0; i<dif;i++){
+            const temp = resultados(tempX, tempY, eixoX[tamY + i]);
+
+            regressoes.push(temp)
+        }
+
+
+        const novoY = tempY.concat(regressoes)
+
+        console.log(`eixo X: ${eixoX}`);
+        console.log(`eixo Y: ${novoY}`);
+    
+
+
+
+    }
+    
+    
+}
+
+
+linearRegression(
+    [1,2,3,4,5,6,7],
+    [10,9,8]
+)
+
+//console.log("efeifhiefeif");
