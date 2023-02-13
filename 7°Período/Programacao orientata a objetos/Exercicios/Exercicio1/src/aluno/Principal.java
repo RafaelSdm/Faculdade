059512063cf3771088a17b66ab2cf644df668eb9
RafaelSdm <rafaeldamasceno2000@gmail.com>
2023-02-13 20:50:24 -0300

    Update Principal.java

--- 7°Período/Programacao orientata a objetos/Exercicios/Exercicio1/src/aluno/Principal.java
+++ 7°Período/Programacao orientata a objetos/Exercicios/Exercicio1/src/aluno/Principal.java
@@ -11,11 +11,42 @@ public class Principal {
 		Date aluno_1 = formato.parse("08/08/2001");
 		Aluno aluno1 = new Aluno("Rafael", "123456789", aluno_1, "rafaeldamasceno2000@gmail.com" );
 		
+		
+		System.out.println("------------------------------------------------------------");
 	
 		System.out.println("Nome:"+ aluno1.getNome());
 		System.out.println("CPF:" + aluno1.getCpf());
 		System.out.println("Data:" +aluno1.getDataNasci());
 		System.out.println("Email:" +aluno1.getEmail());
+		
+		
+		Date aluno_2 = formato.parse("08/08/2001");
+		Aluno aluno2 = new Aluno("Joao", "124569878", aluno_2, "joaokleber@gmail.com" );
+		
+		System.out.println("----------------------------------------------------------");
+		System.out.println("Nome:"+ aluno2.getNome());
+		System.out.println("CPF:" + aluno2.getCpf());
+		System.out.println("Data:" +aluno2.getDataNasci());
+		System.out.println("Email:" +aluno2.getEmail());
+		
+		
+		Date aluno_3 = formato.parse("08/08/2001");
+		Aluno aluno3 = new Aluno("Ricardo", "789456123", aluno_3, "ricardo2023@gmail.com" );
+		System.out.println("-------------------------------------------------------");
+		System.out.println("Nome:"+ aluno3.getNome());
+		System.out.println("CPF:" + aluno3.getCpf());
+		System.out.println("Data:" +aluno3.getDataNasci());
+		System.out.println("Email:" +aluno3.getEmail());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 		
 	}
 }
