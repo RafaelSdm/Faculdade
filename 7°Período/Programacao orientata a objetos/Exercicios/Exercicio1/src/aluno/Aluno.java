5c0f1f43d7275ffd551f03d6c9871122c55a6e7c
RafaelSdm <rafaeldamasceno2000@gmail.com>
2023-02-13 20:44:12 -0300

    commit full course

---
+++ 7°Período/Programacao orientata a objetos/Exercicios/Exercicio1/src/aluno/Aluno.java
@@ -0,0 +1,58 @@
+package aluno;
+
+import java.util.Date;
+
+public class Aluno {
+	
+	private String nome;
+	private String cpf;
+	private Date dataNasci;
+	private String email;
+	
+	
+	public Aluno(String nome, String cpf, Date dataNasci, String email) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNasci = dataNasci;
+		this.email = email;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+		
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public Date getDataNasci() {
+		return dataNasci;
+	}
+	
+	public void setDataNasci(Date dataNasci) {
+		this.dataNasci = dataNasci;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Integer idadeAluno(Aluno aluno) {
+		Integer idade = 0;
+		return idade;
+	}
+
+}
--- 7°Período/Programacao orientata a objetos/Exercicios/Exercicio1/src/aluno/Principal.java
+++ 7°Período/Programacao orientata a objetos/Exercicios/Exercicio1/src/aluno/Principal.java
@@ -1,8 +1,21 @@
 package aluno;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+
 public class Principal {
-	public static void main(String[] args) {
-		System.out.println("Informações do Carro: \n");
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat formato = new SimpleDateFormat("DD/MM/YYYY");
+		Date aluno_1 = formato.parse("08/08/2001");
+		Aluno aluno1 = new Aluno("Rafael", "123456789", aluno_1, "rafaeldamasceno2000@gmail.com" );
+		
+	
+		System.out.println("Nome:"+ aluno1.getNome());
+		System.out.println("CPF:" + aluno1.getCpf());
+		System.out.println("Data:" +aluno1.getDataNasci());
+		System.out.println("Email:" +aluno1.getEmail());
 		
 	}
 }
