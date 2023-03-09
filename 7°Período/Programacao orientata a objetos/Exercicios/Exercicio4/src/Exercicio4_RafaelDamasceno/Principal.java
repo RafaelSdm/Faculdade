2521fa6458d688e9191ffc00c08b94dfb9fc3cae
RafaelSdm <rafaeldamasceno2000@gmail.com>
2023-03-09 15:29:08 -0300

    Update Principal.java

--- 7°Período/Programacao orientata a objetos/Exercicios/Exercicio4/src/Exercicio4_RafaelDamasceno/Principal.java
+++ 7°Período/Programacao orientata a objetos/Exercicios/Exercicio4/src/Exercicio4_RafaelDamasceno/Principal.java
@@ -17,14 +17,14 @@ public class Principal {
 		Proprietario proprietario = new Proprietario("Rafael", 1, "endereco1", "15307988680");
 		listaProprietario.add(proprietario);
 		
-		proprietario = new Proprietario("Joazinho", 100, "endereco de teste", "15625857");
+		proprietario = new Proprietario("Joazinho", 2, "endereco de teste", "15625857");
 		listaProprietario.add(proprietario);
 		
 		
-		proprietario = new Proprietario("Toninho", 600, "endereco de teste 2", "25896345877");
+		proprietario = new Proprietario("Toninho", 3, "endereco de teste 2", "25896345877");
 		listaProprietario.add(proprietario);
 		
-		proprietario = new Proprietario("Amarild0", 4500, "endereco de teste 3", "458789562");
+		proprietario = new Proprietario("Amarild0", 4, "endereco de teste 3", "458789562");
 		listaProprietario.add(proprietario);
 		
 		
@@ -34,91 +34,93 @@ public class Principal {
 		
 		
 		
-		CaoGato caoGato = new CaoGato("TUBIA", "branco", 20, "felino", "macho", 1 ,"pequeno", 22.58 );
-				//String nome, String cor, Integer idade, String raca, String sexo, String porte, Double peso
-		Animal animal = new Animal("TUBIA", "branco",20 ,"felino", "macho", 1);
+		CaoGato caoGato = new CaoGato("TUBIA", "branco", 20, "felino", "macho", 7 ,"pequeno", 22.58 );
+		Animal animal = new Animal("TUBIA", "branco",20 ,"felino", "macho", 7);
 		listaCaoGato.add(caoGato);
 		
 		listaAnimal.add(animal);
 		
-		caoGato = new CaoGato("FOSFORO", "rosa", 14, "felino", "macho", 2 ,"pequeno", 22.58 );
+		caoGato = new CaoGato("FOSFORO", "rosa", 14, "felino", "macho", 8 ,"pequeno", 22.58 );
 		//String nome, String cor, Integer idade, String raca, String sexo, String porte, Double peso
-		animal = new Animal("FOSFORO", "rosa", 14, "felino", "macho", 2);
+		animal = new Animal("FOSFORO", "rosa", 14, "felino", "macho", 8);
 		listaCaoGato.add(caoGato);
 		listaAnimal.add(animal);
 		
 		
-		caoGato = new CaoGato("SPARK", "preto", 5, "felino", "femea", 6 ,"media", 22.58 );
+		caoGato = new CaoGato("SPARK", "preto", 5, "felino", "femea", 9 ,"media", 22.58 );
 		//String nome, String cor, Integer idade, String raca, String sexo, String porte, Double peso
 		
-		animal = new Animal("SPARK", "preto", 5, "felino", "femea", 6);
+		animal = new Animal("SPARK", "preto", 5, "felino", "femea", 9);
 		listaCaoGato.add(caoGato);
 		listaAnimal.add(animal);
 		
 		
-		caoGato = new CaoGato("DOLLY", "rosa", 21, "felino", "macho", 26 ,"pequeno", 22.58 );
+		caoGato = new CaoGato("DOLLY", "rosa", 21, "felino", "macho", 10 ,"pequeno", 22.58 );
 		//String nome, String cor, Integer idade, String raca, String sexo, String porte, Double peso
 		
-		animal = new Animal("DOLLY", "rosa", 21 , "felino", "macho", 26);
+		animal = new Animal("DOLLY", "rosa", 21 , "felino", "macho", 10);
 		listaCaoGato.add(caoGato);
 		listaAnimal.add(animal);
 		
 
-		Passaro passaro = new Passaro("FENA", "preto", 9, "aguia", "femea", 47, "456");
+		Passaro passaro = new Passaro("FENA", "preto", 9, "aguia", "femea", 11, "456");
 		listaPassaro.add(passaro);
-		animal = new Animal("FENA", "preto", 9, "aguia", "femea", 47);
+		animal = new Animal("FENA", "preto", 9, "aguia", "femea", 11);
 		listaAnimal.add(animal);
 		
-		passaro = new Passaro("URIA", "preto", 7, "aguia", "femea", 56, "456");
-		animal = new Animal("URIA", "preto", 7, "aguia", "femea", 56);
+		passaro = new Passaro("URIA", "preto", 7, "aguia", "femea", 12, "456");
+		animal = new Animal("URIA", "preto", 7, "aguia", "femea", 12);
 		listaPassaro.add(passaro);
 		listaAnimal.add(animal);
 		
 		
 		
-		PertenceAnimal pertence = new PertenceAnimal(1,1);
+		PertenceAnimal pertence = new PertenceAnimal(1,7);
 		
 		listaPertenceAnimal.add(pertence);
 		
-		pertence = new PertenceAnimal(1,2);
+		pertence = new PertenceAnimal(1,8);
 		
 		listaPertenceAnimal.add(pertence);
 		
-		pertence = new PertenceAnimal(100,6);
+		pertence = new PertenceAnimal(1,9);
 		
 		listaPertenceAnimal.add(pertence);
 		
-		pertence = new PertenceAnimal(600,26);
+		pertence = new PertenceAnimal(2,10);
 		
 		listaPertenceAnimal.add(pertence);
 		
-		pertence = new PertenceAnimal(4500,56);
+		pertence = new PertenceAnimal(3,11);
 		
 		listaPertenceAnimal.add(pertence);
 		
-		pertence = new PertenceAnimal(4500,47);
+		pertence = new PertenceAnimal(4,12);
 		
 		listaPertenceAnimal.add(pertence);
 		
 		
 		Collections.sort(listaAnimal);
+		
+		
 		int k = 0;
 		for(k = 0; k<listaAnimal.size(); k ++) {
-			System.out.println("INFORMACAO DOS ANIMAIS");
-			//System.out.println("NOME " + listaAnimal.get(k).getNome());
-			//System.out.println("COR " + listaAnimal.get(k).getCor());
+			System.out.println("INFORMACAO DOS ANIMAIS EM ORDEM CRESCENTE");
+			System.out.println("NOME " + listaAnimal.get(k).getNome());
+			System.out.println("COR " + listaAnimal.get(k).getCor());
 			System.out.println("IDADE " + listaAnimal.get(k).getIdade());
-			//System.out.println("PORTE " + listaAnimal.get(k).getRaca());
-			//System.out.println("PESO " + listaAnimal.get(k).getSexo());
-			//System.out.println("id animal " +listaAnimal.get(k).getCodAnimal());
-			//System.out.println("----------------------------------------------------------");
+			System.out.println("PORTE " + listaAnimal.get(k).getRaca());
+			System.out.println("PESO " + listaAnimal.get(k).getSexo());
+			System.out.println("id animal " +listaAnimal.get(k).getCodAnimal());
+			System.out.println("----------------------------------------------------------");
 			
 
 			
 		}
 		
 		
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		
+		//System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 		
 		/*
 		for(Animal animalFor : listaAnimal) {
@@ -153,102 +155,90 @@ public class Principal {
 		}
 		*/
 		
+		
+		
 		for(Animal animalFor : listaAnimal) {
-			System.out.println("animal: "+ animalFor.getNome());
-			for(PertenceAnimal pertenceAn: listaPertenceAnimal) {				
-				for(Proprietario prop : listaProprietario) {
-					if((prop.getCodCli() == pertenceAn.getCodcli() && animalFor.getCodAnimal() == pertenceAn.getCodAnimal()) ) {
-						//System.out.println("------------------------------------------------------------------------");
-						System.out.println("o dono do animal " + animalFor.getNome() + "do id animal " + animalFor.getCodAnimal() + "é o " + prop.getNome() + "com o id " + prop.getCodCli() + " e com o nome: " + prop.getNome());
-					}else {
-						System.out.println("teste de erro id prop " + prop.getCodCli() + " id pertence " + pertence.getCodcli());
+			System.out.println("Animal: "+ animalFor.getNome());
+			//System.out.println("dono do bicho:");
+			System.out.println();
+			System.out.println();
+			
+
+			for(PertenceAnimal pertenceAn: listaPertenceAnimal) {
+				if(animalFor.getCodAnimal() == pertenceAn.getCodAnimal()) {
+					for(Proprietario prop : listaProprietario) {
+						if((prop.getCodCli() == pertenceAn.getCodcli()) ) {
+							//System.out.println("------------------------------------------------------------------------");
+							//System.out.println("o dono do animal " + animalFor.getNome() + "do id animal " + animalFor.getCodAnimal() + "é o " + prop.getNome() + "com o id " + prop.getCodCli() + " e com o nome: " + prop.getNome());
+							System.out.println("INFORMACAO DOS DONOS DOS ANIMAS");
+							System.out.println("NOME " + prop.getNome());
+							System.out.println("Endereco " + prop.getEndereco());
+							System.out.println("CPF: " + prop.getCpf());
+
+		
+							System.out.println("---------------------------------------");
+							break;
+						}else {
+							//System.out.println("o dono do animal " + animalFor.getNome() + "do id animal " + animalFor.getCodAnimal() + "é o " + prop.getNome() + "com o id " + prop.getCodCli() + " e com o nome: " + prop.getNome());
+						}
 					}
+					
 				}
+			
 					
 				
 			}
 			
-		}
-		
-		System.out.println("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
-		
-		for(Proprietario perAn : listaProprietario) {
-			System.out.println(perAn.getNome());
-		}
-		
-
-		
-		
-		
-		
-		
-		
-		
-		/*
-		
-		System.out.println("INFORMACAO DO PROPRIETARIO");
-		System.out.println("NOME"+ proprietario.getNome());
-		System.out.println("CODCLI" + proprietario.getCodCli());
-		System.out.println("ENDERECO" + proprietario.getEndereco());
-		System.out.println("CPF"+ proprietario.getEndereco());
-		
-		
-		System.out.println("-------------------------------------------");
-		
-		*/
-		
-		
-		//System.out.println("INFORMACAO DO ANIMAL GATO");
-		//System.out.println("NOME" + gato.getNome());
-		//System.out.println("COR" + gato.getCor());
-		//System.out.println("IDADE" + gato.getIdade());
-		//System.out.println("PORTE" + gato.getPorte());
-		//System.out.println("PESO" + gato.getPeso());
-		//System.out.println("id animal" + gato.getCodAnimal());
-		
-		
-		/*
-
-		
-		System.out.println("------------------------------------------");
-		
-		
-		
-		System.out.println("INFORMACAO DO PASSARO");
-		System.out.println("NOME" + passaro.getNome());
-		System.out.println("COR" + passaro.getCor());
-		System.out.println("IDADE" + passaro.getIdade());
-		System.out.println("PESO" + passaro.getRaca());
-		System.out.println("NUMERO ANILHA"+passaro.getNumeroAnilha());
-		System.out.println("id animal" + passaro.getCodAnimal());
-		
-		
-		
-		System.out.println("id da pessoa:" + pertence.getCodcli());
-		System.out.println("id do animal" + pertence.getCodAnimal());
-		
-		*/
-		
-		
-		
-
-		
-		
-		/*
-		int i = 0;
-		System.out.println("lista de todos os donos:");
-		for(i = 0; i < listaPertenceAnimal.size(); i++) {
-			//System.out.println("INFORMACAO DO PROPRIETARIO ");
-			//System.out.println("NOME "+ listaProprietario.get(i).getNome());
-			//System.out.println("CODCLI " + listaProprietario.get(i).getCodCli());
-			//System.out.println("ENDERECO " + listaProprietario.get(i).getEndereco());
-			//System.out.println("CPF "+ listaProprietario.get(i).getEndereco());
-			//System.out.println("-----------------------------------------------------");
+					
+			
 			
 		}
 		
-		*/
 		
+		
+		
+		
+		
+		for(Proprietario prop1 : listaProprietario) {
+			System.out.println("Proprietário: "+ prop1.getNome());
+			//System.out.println("dono do bicho:");
+			System.out.println();
+			System.out.println();
+			
+
+			for(PertenceAnimal pertenceAn: listaPertenceAnimal) {
+				if(prop1.getCodCli() == pertenceAn.getCodcli()) {
+					for(Animal animalAn : listaAnimal) {
+						if((animalAn.getCodAnimal() == pertenceAn.getCodAnimal()) ) {
+							//System.out.println("------------------------------------------------------------------------");
+							//System.out.println("o dono do animal " + animalFor.getNome() + "do id animal " + animalFor.getCodAnimal() + "é o " + prop.getNome() + "com o id " + prop.getCodCli() + " e com o nome: " + prop.getNome());
+							System.out.println("INFORMACAO DOS ANIMAIS DE CADA DONO");
+							System.out.println("NOME " + animalAn.getNome());
+							System.out.println("COR " + animalAn.getCor());
+							System.out.println("IDADE " + animalAn.getIdade());
+							System.out.println("PORTE " + animalAn.getRaca());
+							System.out.println("PESO " + animalAn.getSexo());
+							System.out.println("id animal " +animalAn.getCodAnimal());
+							System.out.println("---------------------------------------");
+							break;
+						}else {
+							//System.out.println("o dono do animal " + animalFor.getNome() + "do id animal " + animalFor.getCodAnimal() + "é o " + prop.getNome() + "com o id " + prop.getCodCli() + " e com o nome: " + prop.getNome());
+						}
+					}
+					
+				}
+			
+					
+				
+			}
+			
+					
+			
+			
+		}
+		
+		
+
 		
 	}
 	
