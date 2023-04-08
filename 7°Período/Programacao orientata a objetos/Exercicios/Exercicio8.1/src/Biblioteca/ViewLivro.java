9d95cca25f944f639afcf048547be257eccd140a
RafaelSdm <rafaeldamasceno2000@gmail.com>
2023-04-08 15:40:08 -0300

    Update ViewLivro.java

--- 7°Período/Programacao orientata a objetos/Exercicios/Exercicio8.1/src/Biblioteca/ViewLivro.java
+++ 7°Período/Programacao orientata a objetos/Exercicios/Exercicio8.1/src/Biblioteca/ViewLivro.java
@@ -1,5 +1,93 @@
 package Biblioteca;
 
-public class ViewLivro {
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ViewLivro  extends JFrame{
+	JTextField id, nome, autor, ano_publicacao;
+	
+	JLabel visor;
+	
+	JButton botao;
+	
+	
+	public ViewLivro() {
+		
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+	    
+	    JLabel operador = new JLabel("Informe o id do livro");
+	    add(operador);
+
+	    id = new JTextField(8);
+	    add(id);
+	    
+	    operador = new JLabel("Informe o nome do livro:");
+	    add(operador);
+	    nome = new JTextField(8);
+	    add(nome);
+	    
+	    
+	    operador = new JLabel("Informe o autor do livro");
+	    add(operador);
+	    
+	    autor = new JTextField(8);
+	    add(autor);
+	    
+	    
+	    operador = new JLabel("Informe o ano de publicacao do livro");
+	    add(operador);
+	    
+	    ano_publicacao =  new JTextField(8);
+	    add(ano_publicacao);
+	    
+	    
+	    visor = new JLabel("fodase");
+	    add(visor);
+	    
+	    botao = new JButton("Cadastrar Livro");
+	    add(botao);
+	    
+	    
+
+	    pack();
+	    setVisible(true);
+		
+	}
+	
+	
+	public String getid(){
+		  return id.getText();
+	}
+	public String getnome(){
+		  return nome.getText();
+	}
+	
+	public String getautor() {
+		return autor.getText();
+	}
+	
+	public String getanopublicacao() {
+		return ano_publicacao.getText();
+	}
+	
+	
+	public void setBotaoBehavior(ActionListener botaoBehavior){
+		    botao.addActionListener(botaoBehavior);
+	}
+
+	public void setVisor(String result){
+		    visor.setText(result);
+	}
+	
+
+	
+	
+	
 
 }
