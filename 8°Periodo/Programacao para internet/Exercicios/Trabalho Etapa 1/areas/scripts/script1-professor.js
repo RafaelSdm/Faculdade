c57a35bbd8481d7a827f4cbdc53e3c3782041158
RafaelSdm <rafaeldamasceno2000@gmail.com>
2023-09-28 21:01:31 -0300

    commit full course

--- 8°Periodo/Programacao para internet/Exercicios/Trabalho Etapa 1/areas/scripts/script1-professor.js
+++ 8°Periodo/Programacao para internet/Exercicios/Trabalho Etapa 1/areas/scripts/script1-professor.js
@@ -1,4 +1,3 @@
-        // Função para carregar disciplinas e alunos do localStorage
         function carregarDisciplinasAlunos() {
             const disciplinas = JSON.parse(localStorage.getItem("disciplinas")) || [];
             const alunos = JSON.parse(localStorage.getItem("alunos")) || [];
@@ -21,18 +20,15 @@
             });
         }
 
-        // Função para cadastrar notas e frequências
         function cadastrarNotaFrequencia() {
             const disciplina = document.getElementById("disciplina").value;
             const aluno = document.getElementById("aluno").value;
             const nota = parseFloat(document.getElementById("nota").value);
             const frequencia = parseFloat(document.getElementById("frequencia").value);
 
-            // Aqui você pode salvar as notas e frequências para o aluno e a disciplina selecionados
-            // Lembre-se de criar uma estrutura de dados adequada para isso (por exemplo, um objeto ou matriz)
+
 
             alert(`Nota e frequência cadastradas para ${aluno} na disciplina ${disciplina}.`);
         }
 
-        // Carregar disciplinas e alunos quando a página carregar
         window.addEventListener("load", carregarDisciplinasAlunos);
